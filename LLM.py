8d73153c0b90fc83c77d58ea386113394e967049
sushilkumarmodi <sushilct25@gmail.com>
2025-02-12 11:38:51 +0530

    Update LLM.py

--- LLM.py
+++ LLM.py
@@ -3,7 +3,7 @@ from langchain_groq import ChatGroq
 llm = ChatGroq(
     temperature=0,
     groq_api_key='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',
-    model_name="llama-3.1-70b-versatile"
+    model_name="llama-3.3-70b-versatile"
 )
 
 
